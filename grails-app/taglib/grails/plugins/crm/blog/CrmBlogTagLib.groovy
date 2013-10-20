be0d5b0c98de2a9ff7bc8fcfa2297d8d80aaf54b
Goran Ehrsson <goran@technipelago.se>
2013-10-20 23:32:19 +0200

    Added blogPost tag that renders a single post

--- grails-app/taglib/grails/plugins/crm/blog/CrmBlogTagLib.groovy
+++ grails-app/taglib/grails/plugins/crm/blog/CrmBlogTagLib.groovy
@@ -18,14 +18,20 @@ class CrmBlogTagLib {
         TenantUtils.withTenant(tenant) {
             def query = attrs.query ?: [:]
             def params = attrs.params ?: [:]
-            if(query.status == null) {
+            if (!params.sort) {
+                params.sort = 'date'
+            }
+            if (!params.order) {
+                params.order = 'desc'
+            }
+            if (query.status == null) {
                 query.status = (grailsApplication.config.crm.blog.defaultStatus ?: 'published')
             }
             def result = crmBlogService.list(query, params)
             int i = 0
             for (post in result) {
                 def map = post.dao
-                map.template = {crmContentService.findResourcesByReference(post, [name: (attrs.template ?: 'content.html')]).find{it}}
+                map.template = { crmContentService.findResourcesByReference(post, [name: (attrs.template ?: 'content.html')]).find { it } }
                 def model = [(attrs.var ?: 'it'): map]
                 if (attrs.status) {
                     model[attrs.status] = i++
@@ -33,5 +39,16 @@ class CrmBlogTagLib {
                 out << body(model)
             }
         }
+    }
+
+    def blogPost = { attrs, body ->
+        def post = attrs.post
+        if (!post) {
+            throwTagError("Tag [blogPost] is missing required attribute [post]")
+        }
+        def map = post.dao
+        map.template = { crmContentService.findResourcesByReference(post, [name: (attrs.template ?: 'content.html')]).find { it } }
+        def model = [(attrs.var ?: 'it'): map]
+        out << body(model)
     }
 }
