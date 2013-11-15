56daa933053db6db2782b71d20d2238f8b6deb88
Goran Ehrsson <goran@technipelago.se>
2013-11-15 20:29:53 +0100

    Version 1.0.3 adds REST API support (CrmBlogApiController)

--- CrmBlogGrailsPlugin.groovy
+++ CrmBlogGrailsPlugin.groovy
@@ -17,7 +17,7 @@
 
 class CrmBlogGrailsPlugin {
     def groupId = "grails.crm"
-    def version = "1.0.2"
+    def version = "1.0.3"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['crmContent']
