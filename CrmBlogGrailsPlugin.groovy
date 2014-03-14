1975599a171f56cc062cdc02010050c04b939a95
Goran Ehrsson <goran@technipelago.se>
2014-03-14 13:29:17 +0100

    Version 1.2.1

--- CrmBlogGrailsPlugin.groovy
+++ CrmBlogGrailsPlugin.groovy
@@ -17,7 +17,7 @@
 
 class CrmBlogGrailsPlugin {
     def groupId = "grails.crm"
-    def version = "1.2.0"
+    def version = "1.2.1"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['crmContent']
