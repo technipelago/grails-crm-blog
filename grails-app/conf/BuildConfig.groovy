275c07763a8229a76ce93c3669e922398248e8f0
Goran Ehrsson <goran@technipelago.se>
2018-08-14 14:24:51 +0200

    Upgrade dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -40,7 +40,7 @@ grails.project.dependency.resolution = {
         test(":codenarc:0.25.2") { export = false }
         test(":code-coverage:2.0.3-3") { export = false }
 
-        compile ":crm-content:2.4.4"
+        compile ":crm-content:2.4.5"
     }
 }
 
