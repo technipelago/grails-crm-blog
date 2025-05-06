2a6aee190d3856b25fddc8e4e648ade521de50ca
Goran Ehrsson <goran@technipelago.se>
2025-05-06 20:51:12 +0200

    feat: New servers 2025

--- CrmBlogGrailsPlugin.groovy
+++ CrmBlogGrailsPlugin.groovy
@@ -17,7 +17,7 @@
 
 class CrmBlogGrailsPlugin {
     def groupId = ""
-    def version = "2.4.2-SNAPSHOT"
+    def version = "2.4.2"
     def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def loadAfter = ['crmContent']
